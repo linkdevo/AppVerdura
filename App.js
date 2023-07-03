cc7db95b180bcd4524b6b1eff9b75498e77668a0
Alexandre Link <linkxan@gmail.com>
2023-07-03 06:38:41 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useCallback } from 'react';
+import { SafeAreaView, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import * as SplashScreen from 'expo-splash-screen';
+
+
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fonteCarregada) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fonteCarregada]);
+
+  if(!fonteCarregada) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView onLayout={onLayoutRootView}>
+      <StatusBar />
+      <Cesta {...mock} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
